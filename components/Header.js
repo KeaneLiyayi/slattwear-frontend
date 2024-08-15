77849a061e265ddaae2e8a11183753649ed4935d
Keane <keaneclement40@gmail.com>
2024-08-15 12:24:59 +0300

    Login error

--- components/Header.js
+++ components/Header.js
@@ -102,7 +102,7 @@ const Header = () => {
 
                             ) : (
                                 <div>
-                                    <button onClick={() => signIn()}><svg
+                                    <button onClick={() => router.push('/login')}><svg
                                         xmlns="http://www.w3.org/2000/svg"
                                         fill="none"
                                         viewBox="0 0 24 24"
